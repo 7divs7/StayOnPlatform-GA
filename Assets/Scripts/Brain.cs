b5ac1e4c05a575b90471777525c2073149137f85
Divyani Panda <divyani.panda@gmail.com>
2021-12-30 12:41:09 +0530

    Add files via upload

---
+++ Assets/Scripts/Brain.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    int DNALength = 2; //2 decisions to be made (when you see and when you don't see platform)
+    public float timeAlive; //fitness func based on how long we can stay alive
+    public float timeWalking; //make sure that they don't stay in one place and turn
+    public DNA dna;
+    public GameObject eyes;// use eyes to determine if we are on platform or not
+    private bool alive = true;
+    private bool canSeeGround = true;
+
+    public GameObject ethanPrefab;
+    GameObject ethan;
+
+    void OnDestroy()
+    {
+        Destroy(ethan);
+    }
+
+    void OnCollisionEnter(Collision obj)
+    {
+        //collide with black cube and you are dead
+        Debug.Log("off ground");
+        if(obj.gameObject.tag == "dead")
+        {
+            alive = false;
+            this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+    public void Init()
+    {
+        //dna length = 2, max value of genes = 3
+        // (if see platform)0/1/2 || (if can't see platform)0/1/2
+        //initlialize dna
+        //0 --> forward
+        //1 --> left
+        //2 --> right
+        dna = new DNA(DNALength, 3);
+        timeAlive = 0;
+        alive = true;
+        ethan = Instantiate(ethanPrefab, this.transform.position, this.transform.rotation);
+        ethan.GetComponent<UnityStandardAssets.Characters.ThirdPerson.AICharacterControl>().target = this.transform;
+    }
+
+    void Update()
+    {
+        if(!alive) return; //if bot is dead then don't do anything
+
+        Debug.DrawRay(eyes.transform.position, eyes.transform.forward * 10, Color.red, 10);
+        canSeeGround = false;
+        RaycastHit hit;
+        if(Physics.Raycast(eyes.transform.position, eyes.transform.forward * 10, out hit))
+        {
+            if(hit.collider.gameObject.tag == "platform")
+            {
+                canSeeGround = true;
+            }
+        }
+        timeAlive = PopulationManager.elapsed;
+
+        //read DNA
+        float turn = 0; //turn left or right
+        float move = 0; //forward
+        if(canSeeGround)
+        {
+            //turn character appropriately and always move forward 
+            if(dna.GetGene(0) == 0) {move = 1; timeWalking += 1;} //timewalking happens only when moving
+            else if(dna.GetGene(0) == 1) turn = -90;
+            else if(dna.GetGene(0) == 2) turn = 90;
+        }
+        else
+        {
+            if(dna.GetGene(1) == 0) {move = 1; timeWalking += 1;}
+            else if(dna.GetGene(1) == 1) turn = -90;
+            else if(dna.GetGene(1) == 2) turn = 90;
+        }
+
+        this.transform.Rotate(0f, turn, 0f);
+        this.transform.Translate(0f, 0f, move * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PopulationManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PopulationManager : MonoBehaviour
+{
+    public GameObject botPrefab;
+    public int PopulationSize = 50;
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0;
+    public float trialTime = 10;
+    int generation = 1;
+
+    GUIStyle guiStyle = new GUIStyle();
+    void OnGUI()
+    {
+        guiStyle.fontSize = 25;
+        guiStyle.normal.textColor = Color.white;
+        GUI.BeginGroup(new Rect(10, 10, 250, 150));
+        GUI.Box(new Rect(0, 0, 140, 140), "Stats", guiStyle);
+        GUI.Label(new Rect(10, 25, 250, 30), "Gen: " + generation, guiStyle);
+        GUI.Label(new Rect(10, 50, 250, 30), string.Format("Time: {0:0.00}", elapsed), guiStyle);
+        GUI.Label(new Rect(10, 75, 250, 30), "Population: " + population.Count, guiStyle);
+        GUI.EndGroup();
+    }
+
+    // Start is called before the first frame update, USED for initialization
+    void Start()
+    {
+        for(int i = 0; i < PopulationSize; i++)
+        {
+            Vector3 startingPos = new Vector3(this.transform.position.x + Random.Range(-2, 2), this.transform.position.y, this.transform.position.z + Random.Range(-2, 2));
+            GameObject go = Instantiate(botPrefab, startingPos, this.transform.rotation);
+            go.GetComponent<Brain>().Init();
+            population.Add(go);
+        }
+    }
+
+    GameObject Breed(GameObject parent1, GameObject parent2)
+    {
+        Vector3 startingPos = new Vector3(this.transform.position.x + Random.Range(-2, 2), this.transform.position.y, this.transform.position.z + Random.Range(-2, 2));
+        GameObject offspring = Instantiate(botPrefab, startingPos, this.transform.rotation);
+        Brain b = offspring.GetComponent<Brain>();
+        b.Init();
+        if(Random.Range(0, 100) == 1) // mutate 1 in 100
+        {
+            b.dna.Mutate();
+        }
+        else
+        {
+            b.dna.Crossover(parent1.GetComponent<Brain>().dna, parent2.GetComponent<Brain>().dna);
+        }
+        return offspring;
+    }
+
+    void BreedNewPopulation()
+    {
+        List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<Brain>().timeWalking * o.GetComponent<Brain>().timeAlive).ToList();
+
+        population.Clear();
+        for(int i = (int)(sortedList.Count / 2.0f) - 1; i < sortedList.Count -1; i++)
+        {
+            population.Add(Breed(sortedList[i], sortedList[i+1]));
+            population.Add(Breed(sortedList[i+1], sortedList[i]));
+        }
+
+        //destroy all parent and previous population
+        for(int i = 0; i < sortedList.Count; i++)
+        {
+            Destroy(sortedList[i]);
+        }
+        generation++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+        if(elapsed >= trialTime)
+        {
+            BreedNewPopulation();
+            elapsed = 0;
+        }
+    }
+}
